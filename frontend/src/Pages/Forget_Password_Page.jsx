efdff7fc761b270ef5452d9c7e8f099653781f09
Codex-Swapnil1 <swapnilparchakewrd2@gmail.com>
2024-01-11 14:05:41 +0530

    Basis React Project Setup

---
+++ frontend/src/Pages/Forget_Password_Page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Forget_Password_Page = () => {
+  return (
+    <div>Forget_Password_Page</div>
+  )
+}
+
+export default Forget_Password_Page
---
+++ frontend/src/Pages/Reset_Password_Page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Reset_Password_Page = () => {
+  return (
+    <div>Reset_Password_Page</div>
+  )
+}
+
+export default Reset_Password_Page
